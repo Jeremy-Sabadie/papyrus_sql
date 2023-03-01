a1b1901cb12ed64ede3a3641a4b7d81593c20e2e
cda07saba <jeremy.sabadie@2isa.org>
2023-03-01 09:33:21 +0100

    Q7 done.

--- PAPYRUS_Requetes.sql
+++ PAPYRUS_Requetes.sql
@@ -45,8 +45,9 @@ SELECT  DISTINCT f.NUMFOUR, f.NOMFOUR
 FROM FOURNISSEUR f  JOIN COMMANDE c ON c.NUMFOUR = f.NUMFOUR
 
 -- Si en plus on veut en plus afficher le nombre de commandes
-SELECT  DISTINCT f.NUMFOUR, f.NOMFOUR, COUNT(*) AS nB_cmd 
-FROM FOURNISSEUR f  JOIN COMMANDE c ON c.NUMFOUR = f.NUMFOUR
+SELECT DISTINCT f.NUMFOUR , f.NOMFOUR  ,COUNT(*) AS NbCMD
+FROM FOURNISSEUR f  JOIN COMMANDE c ON f.NUMFOUR =c.NUMFOUR  
+GROUP BY f.NUMFOUR
 -- ==============================================================================================
 -- 8.	Calculer le total de chaque commande. 
 --      Pour chaque commande, afficher le num�ro de commande, le total
